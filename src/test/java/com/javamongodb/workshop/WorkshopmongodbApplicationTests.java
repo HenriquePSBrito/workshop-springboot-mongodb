443db24935babc78efbdd2dab2a80a30a5c03aee
H3nrikito <jhowhp@yahoo.com.br>
2021-10-27 14:45:39 -0300

    project created

---
+++ src/test/java/com/javamongodb/workshop/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javamongodb.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
