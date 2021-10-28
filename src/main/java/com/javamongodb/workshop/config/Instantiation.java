7a2a77407edab604f9c09198b1524c8a4e7c2834
H3nrikito <jhowhp@yahoo.com.br>
2021-10-28 16:26:51 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/javamongodb/workshop/config/Instantiation.java
+++ src/main/java/com/javamongodb/workshop/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.javamongodb.workshop.domain.Post;
 import com.javamongodb.workshop.domain.User;
+import com.javamongodb.workshop.dto.AuthorDTO;
 import com.javamongodb.workshop.repository.PostRepository;
 import com.javamongodb.workshop.repository.UserRepository;
 
@@ -31,16 +32,16 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.deleteAll(); //vai zerar a coleção no mongodb
 		postRepository.deleteAll(); //limpar todos os posts no inicio do programa
-	
-		
+			
 		User gabi = new User(null, "Gabi Martins", "gabi@gmail.com");
 		User josy = new User(null, "Josy Brito", "josydsb@gmail.com");
 		User larissa = new User(null, "Larissa Macedo", "larissa@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("28/06/2019"), "Partiu viagem", "Vou viajar para o Rio de Janeiro. Abraços!", gabi);
-		Post post2 = new Post(null, sdf.parse("30/06/2019"), "Bom dia!", "Acordei feliz hoje!", gabi);
-
 		userRepository.saveAll(Arrays.asList(gabi, josy, larissa));
+		
+		Post post1 = new Post(null, sdf.parse("28/06/2019"), "Partiu viagem", "Vou viajar para o Rio de Janeiro. Abraços!", new AuthorDTO(gabi));
+		Post post2 = new Post(null, sdf.parse("30/06/2019"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(gabi));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
