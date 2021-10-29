489b8e2952de556004b43a774f4a4e8716f74811
H3nrikito <jhowhp@yahoo.com.br>
2021-10-29 14:28:09 -0300

    Consulta com vários critérios

--- src/main/java/com/javamongodb/workshop/services/PostService.java
+++ src/main/java/com/javamongodb/workshop/services/PostService.java
@@ -1,5 +1,6 @@
 package com.javamongodb.workshop.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javamongodb.workshop.domain.Post;
-import com.javamongodb.workshop.domain.User;
 import com.javamongodb.workshop.repository.PostRepository;
 import com.javamongodb.workshop.services.exception.ObjectNotFoundException;
 
@@ -28,5 +28,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return postRepository.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 }
